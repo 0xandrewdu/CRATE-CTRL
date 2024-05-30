7af3cdc7602a8d37f1ca2eea5b6185971b5e7d49
0xandrewdu <andrewyingdu@gmail.com>
2024-05-29 23:50:35 -0700

    gn

--- loss.py
+++ loss.py
@@ -19,6 +19,7 @@ def rate_reduction(
         ZT: torch.Tensor, # b x n x d
         ZTU: torch.Tensor, # b x h x n x d
         eps: float = 0.01,
+        normalize: bool = False,
     ) -> torch.Tensor:
     """
     Computes the rate reduction, defined as the coding rate of the tokens minus sum of the coding 
@@ -27,18 +28,23 @@ def rate_reduction(
     subspaces rotated by the dictionary--should in theory be the same as just using the attention
     layer output, but matters when adding in a sparsity penalization term below)
     """
+    if normalize:
+        ZT, ZTU = F.layer_norm(ZT, ZT.shape[-2:]), F.layer_norm(ZTU, ZTU.shape[-2:])
     return coding_rate(ZT, eps=eps) - torch.sum(coding_rate(ZTU, eps=eps), dim=1)
 
 def sparse_rate_reduction(
         ZT: torch.Tensor, # b x n x d
         ZTU: torch.Tensor, # b x h x n x d
         eps: float = 0.01,
-        lambd: float = 0.1
+        lambd: float = 0.1,
+        normalize: bool = False,
     ) -> torch.Tensor:
     """
     Adds a LASSO penalization term to the above rate reduction function
     TODO: Check that there isn't weird stuff going on with the scale of ZT, ZTU and the l1 penalty. I
-    feel like adding a layernorm with no learnable scale / shift might be the play. There might be some
+    feel like adding a layernorm with no learnable scale / shift could be useful here. There might be some
     weird stuff going on in the RR term since epsilon isn't scale invariant either?
     """
+    if normalize:
+        ZT, ZTU = F.layer_norm(ZT, ZT.shape[-2:]), F.layer_norm(ZTU, ZTU.shape[-2:])
     return rate_reduction(ZT, ZTU, eps=eps) + lambd * ZT.abs().sum(dim=(-1, -2))
