2daf0b1d1983180b32a2c55af16ce3f95d99c892
0xandrewdu <andrewyingdu@gmail.com>
2024-06-02 10:48:45 -0700

    save work

--- models/__init__.py
+++ models/__init__.py
@@ -14,20 +14,20 @@ from timm.models.vision_transformer import PatchEmbed
 
 class CRATE_CTRL_AE:
     def __init__(self, 
-                 depth=16, num_heads=8, dim_head=8, dropout=0., step_size=1., 
-                 image_size=32, patch_size=4, in_channels=3, embed_dim=48
+                 dim=None, depth=16, num_heads=8, dropout=0., step_size=1., 
+                 image_size=32, patch_size=4, in_channels=3,
         ) -> None:
         super().__init__()
 
-
         # set up patch embedding
         assert image_size % patch_size == 0, "image dimensions must be compatible with patch size"
-        dim = image_size // patch_size
-        self.patch_embed = PatchEmbed(img_size=image_size, patch_size=patch_size,in_chans=in_channels, embed_dim=embed_dim)
+        dim = dim or image_size // patch_size
+        self.patch_embed = PatchEmbed(img_size=image_size, patch_size=patch_size,in_chans=in_channels, embed_dim=dim)
 
         # build transformer backbone
         self.encoders = nn.ModuleList([])
         self.decoders = nn.ModuleList([])
+        dim_head = dim // num_heads
         for i in range(depth):
             encoder = CRATE_Transformer_Encode(dim=dim, num_heads=num_heads, dim_head=dim_head, dropout=dropout, step_size=step_size)
             decoder = CRATE_Transformer_Decode(dim=dim, num_heads=num_heads, dim_head=dim_head, dropout=dropout, step_size=step_size)
@@ -38,4 +38,39 @@ class CRATE_CTRL_AE:
             self.decoders.append(decoder)
         self.decoders = self.decoders[::-1]
 
-    def patchify(self, x):
+    def patchify(self, imgs):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        From https://github.com/Ma-Lab-Berkeley/CRATE/blob/main/model/crate_ae/crate_ae.py
+        """
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p ** 2 * 3))
+        return x
+
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        From https://github.com/Ma-Lab-Berkeley/CRATE/blob/main/model/crate_ae/crate_ae.py
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1] ** .5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+    
+    def encode(self, x, training=False):
+        
+
+    def decode(self, x)
+
+    def forward(self, x):
